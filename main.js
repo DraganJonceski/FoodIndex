6966300d520e7d362f3a2df797bb708879433f32
Dragan <jonceski032@gmail.com>
2025-11-26 08:00:10 +0100

    Added Images and more nutrition info

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 
 //1. Hardcoded Food array
 const foods = [
-    { name: "Banana", category: "Fruit", caloriesPer100g: 89, imageName: "banana.png" },
-    { name: "Apple", category: "Fruit", caloriesPer100g: 52, imageName: "apple.png" },
-    { name: "Bread", category: "Grain", caloriesPer100g: 265, imageName: "bread.png" },
-    { name: "Milk", category: "Dairy", caloriesPer100g: 42, imageName: "milk.png" }
+     {
+        name: "Banana",
+        category: "Fruit",
+        caloriesPer100g: 89,
+        protein: 1.1,
+        carbs: 23,
+        fat: 0.3,
+        imageName: "banana.avif" 
+    },
+    {
+        name: "Apple",
+        category: "Fruit",
+        caloriesPer100g: 52,
+        protein: 0.3,
+        carbs: 14,
+        fat: 0.2,
+        imageName: "apple.avif"
+    },
+    { 
+        name: "Bread",
+        category: "Grain", 
+        caloriesPer100g: 265,
+        protein: 2,
+        carbs: 13,
+        fat: 1, 
+        imageName: "bread.avif" },
+    { 
+        name: "Milk", 
+        category: "Dairy", 
+        caloriesPer100g: 42,
+        protein: 8,
+        carbs: 12,
+        fat: 8, 
+        imageName: "milk.avif" }
 ];
 // 2. Render function
 function renderFoods(list){
@@ -12,10 +42,21 @@ function renderFoods(list){
     resultsDiv.innerHTML = "";
 
     list.forEach(food => {
-        const row = document.createElement("p");
-        row.textContent = `${food.name} - ${food.caloriesPer100g} kcal /100g`;
-        resultsDiv.appendChild(row);
+        const card = document.createElement("div");
+        card.className = "food-card";
 
+        card.innerHTML = `<img src="images/${food.imageName}" alt="${food.name}"
+        <h3>${food.name}<h3>
+        <p><strong>${food.caloriesPer100g}</strong> kcal / 100g<p>
+
+        <div class="nutrients">
+            <p>Protein: ${food.protein} g</p>
+            <p>Carbs: ${food.carbs} g</p>
+            <p>Fat: ${food.fat} g</p>
+        </div>
+        `;
+
+        resultsDiv.appendChild(card);
     });
 }
 
